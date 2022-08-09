ca6e03fabb6de99f913cd56f8a481e42329f0caa
Lucas Perez <perezcfnpbnu@gmail.com>
2022-08-08 22:13:54 -0300

    Terminado o exemplo web banco dados (clinica veterinária) com detalhes a ser arrumados

--- Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Controllers/RacaController.cs
+++ Entra21.CSharp.ClinicaVeterinaria/Entra21.CSharp.ClinicaVeterinaria.Aplicacao/Controllers/RacaController.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Entra21.CSharp.ClinicaVeterinaria.Repositorio.BancoDados;
+using Entra21.CSharp.ClinicaVeterinaria.Servico;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Entra21.CSharp.ClinicaVeterinaria.Aplicacao.Controllers
 {
     //Dois pontos herança (mais pra frente)
     public class RacaController : Controller
     {
+        private readonly RacaServico _racaServico;
+
+        //Construtor: Objetivo construir o objeto de RacaController, com o minimo
+        //necessários para o funcionamento correto
+        public RacaController(ClinicaVeterinariaContexto contexto)
+        {
+            _racaServico = new RacaServico(contexto);
+            //racaServico.Cadastrar(nome, especie);
+        }
         /// <summary>
         /// Endpoint que permite listar todas as raças
         /// </summary>
@@ -30,7 +41,9 @@ namespace Entra21.CSharp.ClinicaVeterinaria.Aplicacao.Controllers
             [FromQuery()] string nome,
            [FromQuery()] string especie)
         {
-            return RedirectToAction();
+            _racaServico.Cadastrar(nome, especie);
+
+            return RedirectToAction("index");
         }
     }
 }
